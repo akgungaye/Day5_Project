795d529fcd0dc34a8257ed1e504639e377032c3d
gaye <gungorgaye@gmail.com>
2021-12-25 11:44:18 +0300

    first changes on feature1

--- src/test/java/HelloTest.java
+++ src/test/java/HelloTest.java
@@ -2,5 +2,6 @@ public class HelloTest {
     public static void main(String[] args) {
         System.out.println("hello");
         //add some changes
+        //first change on feature 1
     }
 }
